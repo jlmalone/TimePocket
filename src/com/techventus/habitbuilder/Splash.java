c1ed56e4ec102a512f575bed728284b85e6f1e06
Joseph Malone <malone.joseph@gmail.com>
2013-11-23 23:34:55 -0800

    splash
    splash

---
+++ src/com/techventus/habitbuilder/Splash.java
@@ -0,0 +1,31 @@
+package com.techventus.habitbuilder;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        
+        Handler h = new Handler();
+        h.postDelayed(proceed, 3000);
+        
+        setTitle("TimeFly");
+    }
+    
+    Runnable proceed = new Runnable()
+    {
+
+		@Override
+		public void run() {
+			startActivity(new Intent(Splash.this,TabViewPagerFragmentActivity.class));
+			Splash.this.finish();
+		}
+    	
+    };
+}
