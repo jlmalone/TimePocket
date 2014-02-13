3d5e8e10f59b0e9f59d205b1e898f0bf574306c4
Joseph Malone <malone.joseph@gmail.com>
2014-02-12 23:26:32 -0800

    reformatting

--- src/com/techventus/timefly/IntroAdapter.java
+++ src/com/techventus/timefly/IntroAdapter.java
@@ -12,7 +12,8 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
-public class IntroAdapter extends PagerAdapter {
+public class IntroAdapter extends PagerAdapter
+{
 	int size;
 	Activity act;
 	View layout;
@@ -22,7 +23,7 @@ public class IntroAdapter extends PagerAdapter {
 
 	public void setViewPager(ViewPager pager)
 	{
-		mViewPager =   pager;
+		mViewPager = pager;
 	}
 
 	public void setDialog(Dialog d)
@@ -44,20 +45,20 @@ public class IntroAdapter extends PagerAdapter {
 	}
 
 	@Override
-	public Object instantiateItem(View container, int position) {
-		LayoutInflater inflater = (LayoutInflater) act
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	public Object instantiateItem(View container, int position)
+	{
+		LayoutInflater inflater = (LayoutInflater) act.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		layout = inflater.inflate(R.layout.pages, null);
-		Button b = (Button)layout.findViewById(R.id.next);
+		Button b = (Button) layout.findViewById(R.id.next);
 		b.setOnClickListener(new View.OnClickListener()
 		{
 			@Override
 			public void onClick(View view)
 			{
 				int i = mViewPager.getCurrentItem();
-				if (i  < 5 )
+				if (i < 5)
 				{
-					mViewPager.setCurrentItem(i+1);
+					mViewPager.setCurrentItem(i + 1);
 				}
 				else
 				{
@@ -67,62 +68,65 @@ public class IntroAdapter extends PagerAdapter {
 			}
 		});
 
-		if(position ==0)
+		if (position == 0)
 		{
-			LinearLayout linear = (LinearLayout)inflater.inflate(R.layout.about_1,null) ;
-			((ScrollView)layout.findViewById(R.id.about_container)).removeAllViews();
-			((ScrollView)layout.findViewById(R.id.about_container)).addView(linear);
+			LinearLayout linear = (LinearLayout) inflater.inflate(R.layout.about_1, null);
+			((ScrollView) layout.findViewById(R.id.about_container)).removeAllViews();
+			((ScrollView) layout.findViewById(R.id.about_container)).addView(linear);
 
 		}
-		else if(position==1)
+		else if (position == 1)
 		{
-			LinearLayout linear = (LinearLayout)inflater.inflate(R.layout.about_2,null) ;
-			((ScrollView)layout.findViewById(R.id.about_container)).removeAllViews();
-			((ScrollView)layout.findViewById(R.id.about_container)).addView(linear);
+			LinearLayout linear = (LinearLayout) inflater.inflate(R.layout.about_2, null);
+			((ScrollView) layout.findViewById(R.id.about_container)).removeAllViews();
+			((ScrollView) layout.findViewById(R.id.about_container)).addView(linear);
 
 		}
-		else if(position==2)
+		else if (position == 2)
 		{
-			LinearLayout linear = (LinearLayout)inflater.inflate(R.layout.about_3,null) ;
-			((ScrollView)layout.findViewById(R.id.about_container)).removeAllViews();
-			((ScrollView)layout.findViewById(R.id.about_container)).addView(linear);
+			LinearLayout linear = (LinearLayout) inflater.inflate(R.layout.about_3, null);
+			((ScrollView) layout.findViewById(R.id.about_container)).removeAllViews();
+			((ScrollView) layout.findViewById(R.id.about_container)).addView(linear);
 		}
-		else if(position==3)
+		else if (position == 3)
 		{
-			LinearLayout linear = (LinearLayout)inflater.inflate(R.layout.about_4,null) ;
-			((ScrollView)layout.findViewById(R.id.about_container)).removeAllViews();
-			((ScrollView)layout.findViewById(R.id.about_container)).addView(linear);
+			LinearLayout linear = (LinearLayout) inflater.inflate(R.layout.about_4, null);
+			((ScrollView) layout.findViewById(R.id.about_container)).removeAllViews();
+			((ScrollView) layout.findViewById(R.id.about_container)).addView(linear);
 		}
-		else if(position==4)
+		else if (position == 4)
 
 		{
-			LinearLayout linear = (LinearLayout)inflater.inflate(R.layout.about_5,null) ;
-			((ScrollView)layout.findViewById(R.id.about_container)).removeAllViews();
-			((ScrollView)layout.findViewById(R.id.about_container)).addView(linear);
+			LinearLayout linear = (LinearLayout) inflater.inflate(R.layout.about_5, null);
+			((ScrollView) layout.findViewById(R.id.about_container)).removeAllViews();
+			((ScrollView) layout.findViewById(R.id.about_container)).addView(linear);
 		}
-		else if(position==5)
+		else if (position == 5)
 
 		{
-			LinearLayout linear = (LinearLayout)inflater.inflate(R.layout.about_6,null) ;
-			((ScrollView)layout.findViewById(R.id.about_container)).removeAllViews();
-			((ScrollView)layout.findViewById(R.id.about_container)).addView(linear);
+			LinearLayout linear = (LinearLayout) inflater.inflate(R.layout.about_6, null);
+			((ScrollView) layout.findViewById(R.id.about_container)).removeAllViews();
+			((ScrollView) layout.findViewById(R.id.about_container)).addView(linear);
 		}
 		((ViewPager) container).addView(layout, 0);
 		return layout;
 	}
 
 	@Override
-	public void destroyItem(View arg0, int arg1, Object arg2) {
+	public void destroyItem(View arg0, int arg1, Object arg2)
+	{
 		((ViewPager) arg0).removeView((View) arg2);
 	}
 
 	@Override
-	public boolean isViewFromObject(View arg0, Object arg1) {
+	public boolean isViewFromObject(View arg0, Object arg1)
+	{
 		return arg0 == ((View) arg1);
 	}
 
 	@Override
-	public Parcelable saveState() {
+	public Parcelable saveState()
+	{
 		return null;
 	}
 
